c322303ac4f65a38240b28f55c4e22862cab227c
unggul <unggulsaputra999@.com>
2018-11-30 19:09:57 +0700

    create faker product,review and migrate to database

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'detail'=>$faker->paragraph,
+        'price'=>$faker->numberBetween(100,1000),
+        'stock'=>$faker->numberBetween(1,20),
+        'discount'=>$faker->numberBetween(5,25)
     ];
 });
