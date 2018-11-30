0d951842f59ca0527559d640f683de9421e36b65
unggul <unggulsaputra999@.com>
2018-11-30 19:24:06 +0700

    create relationship product and review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    public function Reviews()
+    {
+    	return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
 
 class Review extends Model
 {
-    //
+    public function Product()
+    {
+    	return $this->belongsTo(Product::class);
+    }
 }
